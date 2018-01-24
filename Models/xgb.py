59d032514d594742a813dee1cc6642d3f8af0006
Ben Greenawald <bgreenawald@gmail.com>
2018-01-23 23:50:37 -0500

    Added oversampling to Al Boraq

--- Models/xgb.py
+++ Models/xgb.py
@@ -7,6 +7,7 @@ from datetime import date
 import numpy as np
 from imblearn.over_sampling import RandomOverSampler
 from collections import Counter
+import pandas as pd
 
 """
 base_dir = "/home/benji/Capstone/Models/exported-features/"
@@ -59,16 +60,23 @@ for group in pr.groups:
     features, response = ros.fit_sample(features, response)
     print(sorted(Counter(response).items()))
 
+    features = pd.DataFrame(features)
+    response = np.array(response)
+
     xg_train = xgb.DMatrix(features, label = response)
-    params = {'max_depth': 10, 'nthread':2, 'n_estimators':500,
+    params = {'max_depth': 10, 'nthread':6, 'n_estimators':500,
     'learning_rate':0.5, 'objective': 'binary:logistic'}
-    bst = xgb.train(params, xg_train, verbose_eval=1)
+    bst = xgb.train(params, xg_train, verbose_eval = 0)
 
     # Read in the test data
     test_features, test_response = pr.readData(group, base_dir, train=False, colLen = features.shape[1])
     print("Test feature shape: " + str(test_features.shape))
     print("Test label length: " + str(len(test_response)))
-    preds = np.array(bst.predict(xgb.DMatrix(test_features)))
+
+    transformed_features = xgb.DMatrix(test_features)
+
+    preds1 = bst.predict(transformed_features)
+    preds = np.array(preds1)
     preds[preds >= 0.5] = 1
     preds[preds < 0.5] = 0
 
